a7aafdef0812d34cf85946fa43e186b349514a04
Joel <joelchu8@gmail.com>
2021-07-20 00:07:26 +0100

    logs in and goes to transfers page

--- Browser.py
+++ Browser.py
@@ -1,4 +1,17 @@
 from selenium import webdriver
 
-browser = webdriver.Edge(r"C:\Users\Joel\Downloads\msedgedriver.exe")
+PATH = r"C:\Users\Joel\Downloads\edgedriver_win64\msedgedriver.exe"
+browser = webdriver.Edge(PATH)
 browser.get('https://fantasy.premierleague.com/')
+
+login_box = browser.find_element_by_id("loginUsername")
+login_box.send_keys("jcevbqlxcvgaijsyuc@rffff.net")
+
+password = str(input("password: "))
+password_box = browser.find_element_by_id("loginPassword")
+password_box.send_keys(password)
+
+password_box.submit()
+
+browser.find_element_by_link_text("Transfers").click()
+# login_button = browser.find_element_by_class_name("Login__LoginButtonWrap-sc-1dpiyoc-3 gJIZgq")
