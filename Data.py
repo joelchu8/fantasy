73589fe2ad9460eaea1c85b48004c5f03c52d593
Joel <joelchu8@gmail.com>
2021-08-18 16:20:35 +0100

    function that gets the top transfer suggestion

--- Data.py
+++ Data.py
@@ -4,35 +4,61 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-#starts a browser session
+# starts a browser session
 PATH = r"C:\Users\Joel\Downloads\edgedriver_win64\msedgedriver.exe"
 browser = webdriver.Edge(PATH)
 browser.get("https://fplreview.com/free-planner/")
 
-#selects highest number of gameweeks to forecast
+# selects highest number of gameweeks to forecast
 select_gw = Select(browser.find_element_by_id("Weeks"))
 gws = select_gw.options
 select_gw.select_by_index(len(gws) - 1)
 
-#input team id
+# input team id
 id_box = browser.find_element_by_name("TeamID")
 id_box.send_keys("1290514")
 id_box.submit()
 
-#wait for overlay to come up and click close button
-WebDriverWait(browser, 10).until(EC.element_to_be_clickable(
-    (By.XPATH, "/html/body/div[2]/div[3]/div/div/div/div[1]/div/article/div[2]/div/div[19]/div/div/div[1]/button")))
-close_button = browser.find_element_by_xpath(
-    "/html/body/div[2]/div[3]/div/div/div/div[1]/div/article/div[2]/div/div[19]/div/div/div[1]/button")
+# wait for overlay to come up and click close button
+close_button_xpath = "/html/body/div[2]/div[3]/div/div/div/div[1]/div/article/div[2]/div/div[19]/div/div/div[1]/button"
+WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, close_button_xpath)))
+close_button = browser.find_element_by_xpath(close_button_xpath)
 close_button.click()
 
-#click transfer suggest button
-transfer_suggest_button = browser.find_element_by_id("transfersuggest")
-transfer_suggest_button.click()
 
-#wait for overlay to come up and click Transfer Suggest
-WebDriverWait(browser, 10).until(EC.element_to_be_clickable(
-    (By.XPATH, "/html/body/div[2]/div[3]/div/div/div/div[1]/div/article/div[2]/div/div[18]/div/div/div[6]/button[1]")))
-transfer_suggester = browser.find_element_by_xpath(
-    "/html/body/div[2]/div[3]/div/div/div/div[1]/div/article/div[2]/div/div[18]/div/div/div[6]/button[1]")
-transfer_suggester.click()
+def suggest_transfers():
+    # click transfer suggest button
+    transfer_suggest_button = browser.find_element_by_id("transfersuggest")
+    transfer_suggest_button.click()
+
+    # wait for overlay to come up and click Transfer Suggest
+    suggester_xpath = "/html/body/div[2]/div[3]/div/div/div/div[1]/div/article/div[2]/div/div[18]/div/div/div[6]/button[1]"
+    WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, suggester_xpath)))
+    transfer_suggester = browser.find_element_by_xpath(suggester_xpath)
+    transfer_suggester.click()
+
+    WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "tradePITCH")))
+    transfers = browser.find_elements_by_id("trade")
+
+    # gets info from top transfer suggestion
+    value = transfers[0].find_element_by_xpath('//*[@id="trade"]/tr[1]/td').text.split()[0]
+    print(value)
+
+    float_value = float(value)
+    print(float_value)
+
+    trades = transfers[0].find_elements_by_class_name("xviPOSROW")
+    players_out = []
+    players_in = []
+    for trade in trades:
+        players = trade.text.split()
+        player_out = players[0]
+        players_out.append(player_out)
+        player_in = players[1]
+        players_in.append(player_in)
+        print(players_out)
+        print(players_in)
+
+
+suggest_transfers()
+
